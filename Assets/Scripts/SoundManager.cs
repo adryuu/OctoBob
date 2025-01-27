398a0a1df943bb15d8ad27bf9b9b0b40acdddf8d
adryuu <ads.040401@gmail.com>
2025-01-28 00:37:19 +0100

    gfdtdt

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -15,18 +15,26 @@ public class AudioManager : MonoBehaviour
     private Slider _musicVolumeSlider;
     [SerializeField]
     private Slider _sfxVolumeSlider;
-
+    bool _isMuted;
+    private float masterVolume, musicVolume, sfxVolume;
     private void Start()
     {
+        _isMuted = false;
         // Inicializar los sliders con los valores actuales del AudioMixer
-        float masterVolume, musicVolume, sfxVolume;
+        
         _audioMixer.GetFloat("MasterVolume", out masterVolume);
         _audioMixer.GetFloat("MusicVolume", out musicVolume);
         _audioMixer.GetFloat("SFXVolume", out sfxVolume);
 
+        _masterVolumeSlider.value = Mathf.Max(Mathf.Log10(masterVolume + 80f) / 80f, 0.0001f);  // Asegura que masterVolume no sea menor a -80 dB
+        _musicVolumeSlider.value = Mathf.Max(Mathf.Log10(musicVolume + 80f) / 80f, 0.0001f);
+        _sfxVolumeSlider.value = Mathf.Max(Mathf.Log10(sfxVolume + 80f) / 80f, 0.0001f);
+
+        /*
         _masterVolumeSlider.value = Mathf.Pow(10, masterVolume / 20); // Convertir de dB a escala 0-1
         _musicVolumeSlider.value = Mathf.Pow(10, musicVolume / 20);
         _sfxVolumeSlider.value = Mathf.Pow(10, sfxVolume / 20);
+        */
 
         // Asignar los eventos a los sliders
         _masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
@@ -34,6 +42,44 @@ public class AudioManager : MonoBehaviour
         _sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
+    
+
+    public void SetMasterVolume(float value)
+    {
+        float dbValue = 20f * Mathf.Log10(value);
+        Debug.Log($"Master volume set to: {dbValue} dB");
+        _audioMixer.SetFloat("MasterVolume", dbValue);
+        PlayerPrefs.SetFloat("MasterVolume", dbValue );
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        float dbValue = 20f * Mathf.Log10(value);
+        Debug.Log($"Music volume set to: {dbValue} dB");
+        _audioMixer.SetFloat("MusicVolume", dbValue);
+        PlayerPrefs.SetFloat("MusicVolume", dbValue);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        float dbValue = 20f * Mathf.Log10(value);
+        Debug.Log($"SFX volume set to: {dbValue} dB");
+        _audioMixer.SetFloat("SFXVolume", dbValue);
+        PlayerPrefs.SetFloat("SFXVolume", dbValue);
+    }
+     public void Mute()
+    {
+        if (_isMuted == false)
+        {
+            _audioMixer.SetFloat("MasterVolume", 0);
+        }
+        else
+        {
+            _audioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
+        }
+        _isMuted = !_isMuted;
+    }
+    /*
     public void SetMasterVolume(float value)
     {
         // Convertir de escala 0-1 a dB y asignar al AudioMixer
@@ -49,6 +95,7 @@ public class AudioManager : MonoBehaviour
     {
         _audioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
     }
+    */
 
     private void OnDestroy()
     {
